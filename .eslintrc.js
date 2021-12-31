9fe2a951d4e1dc2bcba01307bcb4d1b42e88d3d0
Kouts <koutsaftakis@gmail.com>
2021-12-31 18:31:06 +0200

    style: added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'standard',
+    'plugin:import/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  plugins: [
+    'html'
+  ],
+  ignorePatterns: [
+    '**/node_modules/**',
+    '{tmp,temp}/**',
+    '**/*.min.js',
+    'vendor/**',
+    'dist/**',
+    'public/**'
+  ],
+  overrides: [
+    {
+      files: ['*.json'],
+      rules: {
+        quotes: [2, 'double']
+      }
+    },
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      },
+      rules: {
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
+        '@typescript-eslint/no-unused-vars': 'off'
+      }
+    },
+    {
+      files: ['**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true
+      }
+    }
+
+  ],
+  rules: {
+    // Console and debugger settings depending whether we're on production or not
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
