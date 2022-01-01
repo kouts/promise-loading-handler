b2b8701db003570e9ee2e47d06825921e40b672a
Kouts <koutsaftakis@gmail.com>
2022-01-01 12:34:01 +0200

    style: removed rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,11 +30,6 @@ module.exports = {
       parser: 'vue-eslint-parser',
       parserOptions: {
         parser: '@typescript-eslint/parser'
-      },
-      rules: {
-        'no-unused-vars': 'off',
-        'no-undef': 'off',
-        '@typescript-eslint/no-unused-vars': 'off'
       }
     },
     {
