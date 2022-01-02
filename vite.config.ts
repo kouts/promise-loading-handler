5915009da5a4e568c7769bfaf360980aef660f4c
Kouts <koutsaftakis@gmail.com>
2022-01-02 11:06:13 +0200

    build: removed favicon from dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import del from 'rollup-plugin-delete'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [del({ targets: 'dist/favicon.ico', hook: 'writeBundle' }), vue()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
